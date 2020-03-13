7bd1bbad7da2c38cb137232157a235f0d5511dfd
Timothy Chacko <tchacko@uncc.edu>
2020-03-13 18:21:59 -0400

    Client set_name

--- client.py
+++ client.py
@@ -2,7 +2,7 @@ import socket
 import tkinter
 from threading import Thread
 
-HOST = '10.0.0.174'
+HOST = '71.196.93.132'
 PORT = 6667
 
 main_window = tkinter.Tk()
@@ -11,36 +11,34 @@ main_window.title("Chat Lobby")
 name_window = tkinter.Tk()
 
 def set_name():
-    main_window.attributes('-topmost', False)
+    #main_window.attributes('-topmost', False)
 
     name_window.attributes('-topmost', True)
-    name_window.title("")
+    name_window.title("Name")
     name_window.lift()
 
+    prompt = tkinter.Label(name_window, text="Enter your name: ")
+    prompt.pack(side=tkinter.LEFT)
     name = tkinter.StringVar()
     name_entry = tkinter.Entry(name_window, textvariable=name, width=40)
     name_entry.pack(side=tkinter.LEFT)
     name_entry.bind("<Return>", lambda s: send_name(name))
 
-    prompt = tkinter.Label(name_window, text="Enter your name.")
-    prompt.pack()
-
-
 def send_name(name,event=None):
     socket.send(bytes(name.get(), "utf8"))
     name_window.destroy()
-
+    main_window.attributes('-topmost', True)
+    main_window.focus_force()
 
 def receive():
     while True:
         response = socket.recv(1024)
         text_box.insert(tkinter.END, "Server: " + response.decode())
 
-
 def send(event=None):
-        socket.send(bytes(msg.get(), "utf8"))
-        text_box.insert(tkinter.END, "Me: " + msg.get())
-        msg.set("")
+        socket.send(bytes(message.get(), "utf8"))
+        #text_box.insert(tkinter.END, "Me: " + message.get())
+        message.set("")
 
 if __name__ == "__main__":
     text_box = tkinter.Listbox(main_window, height=20, width=100)
@@ -49,8 +47,8 @@ if __name__ == "__main__":
     message = tkinter.StringVar()
 
     entry = tkinter.Entry(main_window, textvariable=message, width=80)
-    entry.bind("<Return>", send)
     entry.pack(side=tkinter.LEFT)
+    entry.bind("<Return>", send)
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket:
         socket.connect((HOST, PORT))
