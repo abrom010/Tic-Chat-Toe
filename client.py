93c7791152c02146fcf1dba31407a59a7b9475e7
Aaron Brombergues <abrom010@fiu.edu>
2020-03-13 18:50:29 -0400

    name wasn't sending

--- client.py
+++ client.py
@@ -2,13 +2,17 @@ import socket
 import tkinter
 from threading import Thread
 
-HOST = '71.196.93.132'
+HOST = '10.0.0.174'
 PORT = 6667
 
 main_window = tkinter.Tk()
 main_window.title("Chat Lobby")
 
 name_window = tkinter.Tk()
+
+
+message = tkinter.StringVar()
+name = tkinter.StringVar()
 
 def set_name():
     #main_window.attributes('-topmost', False)
@@ -19,13 +23,14 @@ def set_name():
 
     prompt = tkinter.Label(name_window, text="Enter your name: ")
     prompt.pack(side=tkinter.LEFT)
-    name = tkinter.StringVar()
-    name_entry = tkinter.Entry(name_window, textvariable=name, width=40)
+
+    name_entry = tkinter.Entry(name_window, width=40)
     name_entry.pack(side=tkinter.LEFT)
-    name_entry.bind("<Return>", lambda s: send_name(name))
+    name_entry.bind("<Return>", lambda s: send_name(name_entry.get()))
 
 def send_name(name,event=None):
-    socket.send(bytes(name.get(), "utf8"))
+    if name == "": name = "guest"
+    socket.send(bytes(name, "utf8"))
     name_window.destroy()
     main_window.attributes('-topmost', True)
     main_window.focus_force()
@@ -36,6 +41,7 @@ def receive():
         text_box.insert(tkinter.END, "Server: " + response.decode())
 
 def send(event=None):
+        print(message.get())
         socket.send(bytes(message.get(), "utf8"))
         #text_box.insert(tkinter.END, "Me: " + message.get())
         message.set("")
@@ -43,8 +49,6 @@ def send(event=None):
 if __name__ == "__main__":
     text_box = tkinter.Listbox(main_window, height=20, width=100)
     text_box.pack()
-
-    message = tkinter.StringVar()
 
     entry = tkinter.Entry(main_window, textvariable=message, width=80)
     entry.pack(side=tkinter.LEFT)
