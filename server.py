e5dcb592a5ca045625ad7ca43cd90669fef9278d
Aaron Brombergues <abrom010@fiu.edu>
2020-03-13 19:54:04 -0400

    socket exceptions

--- server.py
+++ server.py
@@ -4,24 +4,32 @@ import tkinter
 
 HOST = '10.0.0.174'
 PORT = 6667
-                
+connections = {}
+
 def accept_incoming_connections():
     while True:
         connection, addr = server.accept()
-        connection.send(bytes("You have connected, please type in your name: ","utf8"))
+        connection.send(bytes("You have connected, please type in your name.","utf8"))
         Thread(target=handle_client, args=(connection,)).start()
 
 def handle_client(connection):
     with connection:
-        #name = bytes("default","utf8")
         name = connection.recv(1024)
-        #print(name.decode()+" has connected")
         print(name.decode()+" connected")
+        connections.update({connection : name})
         connection.send(bytes("Welcome, "+name.decode()+"!","utf8"))
-        while True: #while connection exists and data is coming
-            data = bytes(name.decode()+": "+connection.recv(1024).decode(),"utf8") #buffer size
-            print((name.decode()+": "+data.decode()))
-            connection.sendall(data) #Python specific function that sends entire
+        try:
+            while True: #while connection exists and data is coming
+                data = bytes(name.decode()+": "+connection.recv(1024).decode(),"utf8") #buffer size
+                print(data.decode())
+                for c in connections.keys():
+                    try:
+                        c.send(data) #Python specific function that sends entire
+                    except:
+                        print(connections[connection]+" left")
+                        del connections[c]
+        except:
+            del connections[connection]
 
 if __name__ == "__main__":           
     with socket(AF_INET, SOCK_STREAM) as server:
