31f04497ed426e43b2cbc649028085a49bb11f6a
Aaron Brombergues <abrom010@fiu.edu>
2020-03-14 14:52:24 -0400

    reorganize

---
+++ copies/client-copy.py
@@ -0,0 +1,60 @@
+# from tkinter import *
+import socket
+import tkinter
+import tkinter.font as font
+from threading import Thread
+
+HOST = '10.0.0.174'
+PORT = 6667
+
+main_window = tkinter.Tk()
+main_window.title("Chat Lobby")
+
+name_window = tkinter.Tk()
+
+message = tkinter.StringVar()
+name = tkinter.StringVar()
+
+def set_name():
+    name_window.attributes('-topmost', True)
+    name_window.title("Name")
+    name_window.lift()
+
+    prompt = tkinter.Label(name_window, text="Enter your name: ")
+    prompt.pack(side=tkinter.LEFT)
+
+    name_entry = tkinter.Entry(name_window, width=40)
+    name_entry.pack(side=tkinter.LEFT)
+    name_entry.bind("<Return>", lambda s: send_name(name_entry.get()))
+
+def send_name(name,event=None):
+    if name == "": name = "guest"
+    socket.send(bytes(name, "utf8"))
+    name_window.destroy()
+    main_window.attributes('-topmost', True)
+    main_window.focus_force()
+    entry = tkinter.Entry(main_window, textvariable=message, width=75, relief=tkinter.FLAT)
+    entry.pack(side=tkinter.LEFT)
+    entry.bind("<Return>", send)
+    buton = tkinter.Button(main_window, text="Send", command=send, width=20, relief=tkinter.FLAT)
+    buton['font'] = font.Font(family='Helvetica', size=12)
+    buton.pack()
+
+def receive():
+    while True:
+        response = socket.recv(1024)
+        text_box.insert(tkinter.END, response.decode())
+
+def send(event=None):
+        socket.send(bytes(message.get(), "utf8"))
+        message.set("")
+
+if __name__ == "__main__":
+    text_box = tkinter.Listbox(main_window, height=20, width=110, relief=tkinter.FLAT)
+    text_box.pack()
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket:
+        socket.connect((HOST, PORT))
+        Thread(target=receive).start()
+        set_name()
+        main_window.mainloop()
---
+++ copies/server-copy1.py
@@ -0,0 +1,46 @@
+from socket import AF_INET, socket, SOCK_STREAM
+from threading import Thread
+import tkinter
+import time
+import pickle
+
+HOST = '10.0.0.174'
+PORT = 6667
+connections = {}
+serverTime = "%H:%M:%S %p"
+chatTime = "%H:%M %p"
+
+def accept_incoming_connections():
+    while True:
+        connection, addr = server.accept()
+        # connection.send(bytes("You have connected, please type in your name.","utf8"))    //Not needed, get name already from client side
+        Thread(target=handle_client, args=(connection,)).start()
+
+def handle_client(connection):
+    with connection:
+        name = connection.recv(1024)
+        print(time.strftime(serverTime) + " Log: " + name.decode()+" connected")
+        connections.update({connection : name})
+        for c in connections.keys():
+        	c.send(bytes(time.strftime(chatTime) + " Server: Welcome, "+name.decode()+"!","utf8"))
+        while True: #while connection exists and data is coming
+            try:
+                data = bytes(time.strftime(chatTime) + " " + name.decode()+": "+connection.recv(1024).decode(),"utf8") #buffer size
+            except:
+                connection.close()
+                del connections[connection]
+                print(time.strftime(serverTime) + " Log: " + name.decode()+" left")
+                for c in connections.keys():
+                    c.send(bytes(time.strftime(chatTime) + " Server: " + name.decode()+" left","utf8"))
+                break
+            for c in connections.keys():
+                    c.send(data) #Python specific function that sends entire
+
+if __name__ == "__main__":
+    with socket(AF_INET, SOCK_STREAM) as server:
+        server.bind((HOST, PORT))
+        server.listen()
+        ACCEPT_THREAD = Thread(target=accept_incoming_connections)
+        ACCEPT_THREAD.start()
+        ACCEPT_THREAD.join()
+        server.close()
