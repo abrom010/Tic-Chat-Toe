98f68ca5bef2aadde6b9ab4b2734243ead055112
Timothy Chacko <tchacko@uncc.edu>
2020-03-14 01:33:41 -0400

    Server outputs

--- server.py
+++ server.py
@@ -1,37 +1,40 @@
 from socket import AF_INET, socket, SOCK_STREAM
 from threading import Thread
 import tkinter
+import time
 
-HOST = '10.0.0.174'
+HOST = '100.64.7.96'
 PORT = 6667
 connections = {}
+serverTime = "%H:%M:%S"
+chatTime = "%H:%M"
 
 def accept_incoming_connections():
     while True:
         connection, addr = server.accept()
-        connection.send(bytes("You have connected, please type in your name.","utf8"))
+        # connection.send(bytes("You have connected, please type in your name.","utf8"))    //Not needed, get name already from client side
         Thread(target=handle_client, args=(connection,)).start()
 
 def handle_client(connection):
     with connection:
         name = connection.recv(1024)
-        print(name.decode()+" connected")
+        print(time.strftime(serverTime) + " Log: " + name.decode()+" connected")
         connections.update({connection : name})
-        connection.send(bytes("Welcome, "+name.decode()+"!","utf8"))
+        connection.send(bytes(strftime(chatTime) + " Server: Welcome, "+name.decode()+"!","utf8"))
         while True: #while connection exists and data is coming
             try:
-                data = bytes(name.decode()+": "+connection.recv(1024).decode(),"utf8") #buffer size
+                data = bytes(strftime(chatTime) + " " + name.decode()+": "+connection.recv(1024).decode(),"utf8") #buffer size
             except:
                 connection.close()
                 del connections[connection]
-                print(name.decode()+" left")
+                print(strftime(serverTime) + " Log: " + name.decode()+" left")
                 for c in connections.keys():
-                    c.send(bytes(name.decode()+" left","utf8"))
+                    c.send(bytes(strftime(chatTime) + " Server: " + name.decode()+" left","utf8"))
                 break
             for c in connections.keys():
                     c.send(data) #Python specific function that sends entire
 
-if __name__ == "__main__":           
+if __name__ == "__main__":
     with socket(AF_INET, SOCK_STREAM) as server:
         server.bind((HOST, PORT))
         server.listen()
