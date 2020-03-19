ab9a8c1d07007cb94f4aa29b91b0898558e703f0
Aaron Brombergues <abrom010@fiu.edu>
2020-03-18 20:25:24 -0400

    shorten

--- toe.py
+++ toe.py
@@ -2,109 +2,55 @@
 class Box:
     isFilled = False
     hasCircle = False
-    isDiagonal = False
 
-    def __init__(self, isDiagonal=False):
-        self.isDiagonal = isDiagonal
-
-    #call with an argument to fill box with o, otherwise with x
+    #call to fill box
     def fill(self,hasCircle=False):
         self.isFilled = True
 
         if hasCircle:
             self.hasCircle = True
-        else:
-            pass
 
 ''' CLASS '''
 
 class Board:
-    square_won = False
+    cross_won = False
     circle_won = False
     game_over = False
 
     upleft,upmid,upright,midleft,center,midright,botleft,botmid,botright = \
-    Box(True),Box(),Box(True),Box(),Box(True),Box(),Box(True),Box(),Box(True)
+    Box(),Box(),Box(),Box(),Box(),Box(),Box(),Box(),Box()
 
     boxes = {"upleft":upleft,"upmid":upmid,"upright":upright,"midleft":midleft,"center":center,"midright":midright,"botleft":botleft,"botmid":botmid,"botright":botright}
+
     rows = [[upleft,upmid,upright],[midleft,center,midright],[botleft,botmid,botright]]
     columns = [[upleft,midleft,botleft],[upmid,center,botmid],[upright,midright,botright]]
+    diagonals = [[upleft,center,botright],[upright,center,botleft]]
+
+    groups = [rows,columns,diagonals]
 
     ''' BIG FUNCTION '''
 
     #checks for game_over
     def check(self):
-        center = self.boxes["center"]
+        for group in self.groups:
+            for boxes in group:
+                ctr = 0
+                first = boxes[0]
 
-        if center.isFilled:
-            searchList = {}
-            for (name,box) in self.boxes.items():
-                if box == center or not box.isDiagonal:
-                    pass
-                else:
-                    if box.isFilled and box.hasCircle == center.hasCircle:
-                        searchList.update({name:box})
-            
-            if len(searchList) > 1:
-                for name in searchList.keys():
-                    win_string = ""
-                    if "up" in name:
-                        win_string+= "bot"
-                    else:
-                        win_string+= "up"
+                if first.isFilled:
+                    for box in boxes:
+                        if box != first and box.isFilled and box.hasCircle == first.hasCircle:
+                            ctr+=1
 
-                    if "left" in name:
-                        win_string+= "right"
-                    else:
-                        win_string+= "left"
-
-                    for name in searchList.keys():
-                        if name == win_string:
-                            if center.hasCircle == True:
+                        if ctr == 2:
+                            if first.hasCircle == True:
                                 self.circle_won = True
                             else:
-                                self.square_won = True
-                        self.game_over = True
-        print(self.game_over)
+                                self.cross_won = True
+                            self.game_over = True
+                            break
 
-        for row in self.rows:
-            ctr = 0
-            first = row[0]
-
-            if first.isFilled:
-                for box in row:
-                    if box == first:
-                        pass
-                    else:
-                        if box.isFilled == first.isFilled and box.hasCircle == first.hasCircle:
-                            ctr+=1
-                    if ctr > 1:
-                        if first.hasCircle == True:
-                            self.circle_won = True
-                        else:
-                            self.square_won = True
-                        self.game_over = True
-                        break
-
-        for column in self.columns:
-            ctr = 0
-            first = column[0]
-
-            if first.isFilled:
-                for box in column:
-                    if box == first:
-                        pass
-                    else:
-                        if box.isFilled == first.isFilled and box.hasCircle == first.hasCircle:
-                            ctr+=1
-                    if ctr > 1:
-                        if first.hasCircle == True:
-                            self.circle_won = True
-                        else:
-                            self.square_won = True
-                        self.game_over = True
-                        break
-        print(self.game_over)
+        return(self.game_over)
 
         ''' BIG FUNCTION '''
 
@@ -112,4 +58,4 @@ class Board:
 
 if __name__ == "__main__":
     board = Board()
-    board.check()
+    print(board.check())
